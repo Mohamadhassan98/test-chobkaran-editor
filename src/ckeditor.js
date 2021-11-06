0f82db3b0ae86034c86a0469cd9cb5c84432d33c
Mohamadhassan Ebrahimi <emohamadhassan@gmail.com>
2021-11-06 20:43:08 +0330

    Added alignment to build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,6 +4,7 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
@@ -38,6 +39,7 @@ class Editor extends ClassicEditor {}
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Autoformat,
+	Alignment,
 	BlockQuote,
 	Bold,
 	Essentials,
